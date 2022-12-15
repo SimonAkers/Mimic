aafc756cc5e9e59bd1e9f47ab9a4f94913b7c4cd
Shayes <hayesakers@gmail.com>
2022-12-15 17:43:03 -0500

    Began work on macros that run a script.

---
+++ src/main/java/net/shayes/midimacros/macros/ScriptMacro.java
@@ -0,0 +1,19 @@
+package net.shayes.midimacros.macros;
+
+import javax.script.*;
+
+public class ScriptMacro extends Macro {
+    public static void main(String[] args) throws ScriptException {
+        ScriptMacro parser = new ScriptMacro("groovy");
+    }
+
+    public ScriptMacro(String lang) throws ScriptException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName(lang);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
