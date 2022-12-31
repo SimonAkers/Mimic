a8337f893efd45d5bad24672902c3fcf151c79f7
Shayes <hayesakers@gmail.com>
2022-12-31 13:32:28 -0500

    Added documentation for ColorDevice.

--- src/main/java/net/shayes/mimic/device/ColorDevice.java
+++ src/main/java/net/shayes/mimic/device/ColorDevice.java
@@ -23,10 +23,28 @@ public abstract class ColorDevice extends Device {
         super(name);
     }
 
+    /**
+     * Loads and applies colors to the device from a TOML file.
+     *
+     * @param file The name (path) of the file.
+     *
+     * @throws MidiUnavailableException If the device is unavailable.
+     * @throws InvalidMidiDataException If the color data is invalid.
+     * @throws IOException If the file could not be accessed.
+     */
     public void loadColors(String file) throws MidiUnavailableException, InvalidMidiDataException, IOException {
         loadColors(Paths.get(file));
     }
 
+    /**
+     * Loads and applies colors to the device from a TOML file.
+     *
+     * @param file The Path object representing the file.
+     *
+     * @throws MidiUnavailableException If the device is not available.
+     * @throws InvalidMidiDataException If the color data is invalid.
+     * @throws IOException If the file could not be accessed.
+     */
     public void loadColors(Path file) throws IOException, MidiUnavailableException, InvalidMidiDataException {
         TomlParseResult result = Toml.parse(file);
 
@@ -44,10 +62,28 @@ public abstract class ColorDevice extends Device {
         }
     }
 
+    /**
+     * Sets the color of a given note.
+     *
+     * @param noteName The name of the note to color.
+     * @param hexColor The color to apply, in hex form (e.g., A4DE67, #FFFFFF)
+     *
+     * @throws MidiUnavailableException If the device is not available.
+     * @throws InvalidMidiDataException If the color data is invalid.
+     */
     public void setColor(String noteName, String hexColor) throws MidiUnavailableException, InvalidMidiDataException {
         setColor(MidiUtils.note(noteName), hexColor);
     }
 
+    /**
+     * Sets the color of a given note.
+     *
+     * @param note The MIDI value of the note to color.
+     * @param hexColor The color to apply, in hex form (e.g., A4DE67, #FFFFFF)
+     *
+     * @throws MidiUnavailableException If the device is not available.
+     * @throws InvalidMidiDataException If the color data is invalid.
+     */
     public void setColor(int note, String hexColor) throws MidiUnavailableException, InvalidMidiDataException {
         hexColor = hexColor.charAt(0) == '#' ? hexColor : '#' + hexColor;
 
@@ -55,5 +91,16 @@ public abstract class ColorDevice extends Device {
         setColor(note, color.getRed(), color.getGreen(), color.getBlue());
     }
 
+    /**
+     * Sets the color of a given note.
+     *
+     * @param note The MIDI value of the note to color.
+     * @param red The red value of the color from 0-255.
+     * @param green The green value of the color from 0-255.
+     * @param blue The blue value of the color from 0-255.
+     *
+     * @throws MidiUnavailableException If the device is not available.
+     * @throws InvalidMidiDataException If the color data is invalid.
+     */
     public abstract void setColor(int note, int red, int green, int blue) throws MidiUnavailableException, InvalidMidiDataException;
 }
